2f8595cd2bc5c499d52ec4c2f4d4d1b69be0d48a
Brian Lewis <brianlewis2021@u.northwestern.edu>
2021-05-07 11:33:35 -0500

    Updated create_db.py with loggers and detailed table info.

--- src/create_db.py
+++ src/create_db.py
@@ -4,6 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 import src.config as config
 
+logging.config.fileConfig(fname=config.LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
+
 
 def create_db(engine_string=config.SQLALCHEMY_DATABASE_URI):
     """
@@ -22,23 +25,24 @@ def create_db(engine_string=config.SQLALCHEMY_DATABASE_URI):
         name = Column(String(200), unique=True, nullable=True)
         latitude = Column(String(100), unique=False, nullable=False)
         longitude = Column(String(100), unique=False, nullable=False)
-        capacity = Column(Integer)
-        num_bikes = Column(Integer)
-        last_reported = Column(DateTime)
+        capacity = Column(Integer, unique=False, nullable=True)
+        num_bikes = Column(Integer, unique=False, nullable=True)
+        last_reported = Column(DateTime, unique=False, nullable=True)
 
         def __repr__(self):
             return '<Stations %r>' % self.title
 
     class trips(Base):
-        """Create a data model for the database to be set up for bike stations """
+        """Create a data model for the database to be set up for bike trips """
         __tablename__ = 'trips'
         station_id = Column(Integer, primary_key=True)
-        date = Column(DateTime)
-        hour = Column(Integer)
-        inflows = Column(Integer)
-        outflows = Column(Integer)
+        date = Column(DateTime, unique=False, nullable=True)
+        hour = Column(Integer, unique=False, nullable=True)
+        inflows = Column(Integer, unique=False, nullable=True)
+        outflows = Column(Integer, unique=False, nullable=True)
 
         def __repr__(self):
             return '<Trips %r>' % self.title
 
+    logger.info("Creating databases using {}.".format(engine_string))
     Base.metadata.create_all(engine)
